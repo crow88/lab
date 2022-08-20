6d8da451f8094ab736cde9e851c371c3de389395
crow88 <crow88.t@gmail.com>
2022-08-20 15:44:04 +0900

    Update 421_2.js

--- 12008305/js/cjs/episode/421_2/421_2.js
+++ 12008305/js/cjs/episode/421_2/421_2.js
@@ -1192,7 +1192,7 @@ p.nominalBounds = new cjs.Rectangle(-2.2,-0.1,4.5,6.8999999999999995);
 		text: "분장실", // 장소 이름
 		font: "20px sans-serif", // 폰트 크기, 폰트 이름
 		color: "#333333", // 색상 RGB 값
-		x: 230, // x 좌표값
+		x: 50, // x 좌표값
 		y: -10, // y 좌표값
 	}
 	this.shape = new cjs.Text(korTitle.text, korTitle.font, korTitle.color);
@@ -2076,7 +2076,7 @@ p.nominalBounds = new cjs.Rectangle(-41.7,-47.9,173.5,72.7);
 		font: "16px sans-serif", // 폰트 크기, 폰트 이름
 		color: "#333333", // 색상 RGB 값
 		x: 18, // x 좌표값
-		y: 10, // y 좌표값
+		y: 4, // y 좌표값
 	}
 	this.shape = new cjs.Text(korTitle.text, korTitle.font, korTitle.color);
 	this.shape.setTransform(korTitle.x, korTitle.y);
